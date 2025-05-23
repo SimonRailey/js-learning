3ddb739aac083bdbea1a2783fa38cbf340b47c7a
Niladri Biswas <nse.niladri@gmail.com>
2025-05-23 15:37:55 +0000

    day 2

---
+++ 01-Basics/02_DataTypeNConversion.js
@@ -0,0 +1,41 @@
+let name="Niladri"
+let age=30
+let isAunthenticaed=false
+let city;
+/*
+Premitive Datatype
+here name => is a string=>""
+age is number=> from 2*10^53
+isAuthenticated=> bool data type
+city => undefined datatype
+null=> is a datatype and is a standalone value, intentionally telling that it is not undefined
+bigint
+symbols=>used mostly in react for uniques
+*/
+
+// to find typeof a datatype we use typeof 'any datatype'
+
+//console.table([typeof name,typeof age,typeof isAunthenticaed,typeof city,typeof null])
+
+// So null datatype is object
+
+/////////////////////// Conversion of datatypes
+
+let score=30
+let score2="22"
+
+// Why conversion is required as if working in front end we want to display it as string but from backend it was passed as number
+// To avoid unambiguity we use this type conversion
+
+// console.log(typeof score);   // number
+// console.log(typeof score2); // sting
+// console.log(typeof Number(score2)); // here score 2 which was declared as string is converted to number datatype
+// console.log(Number(score2))
+
+// Boolean Conversion
+
+// let isLoggedIn = 1
+
+// console.table([Boolean(isLoggedIn),Boolean(""),Boolean("Niladri")]) // True,False,True
+// console.table([String(22),typeof String(22)])
+
